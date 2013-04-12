dc84d053d9bbadc25325431f0397e2186344ead9
Moritz Breit <mail@mobr.de>
2013-04-12 15:58:24 +0200

    Move assets to source/assets

--- config.rb
+++ config.rb
@@ -55,11 +55,11 @@ helpers MapHelpers
 require "lib/event/event_helpers"
 helpers Event::EventHelpers
 
-set :css_dir, 'stylesheets'
+set :css_dir, 'assets/stylesheets'
 
-set :js_dir, 'javascripts'
+set :js_dir, 'assets/javascripts'
 
-set :images_dir, 'images'
+set :images_dir, 'assets/images'
 
 # Build-specific configuration
 configure :build do
