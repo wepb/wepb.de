03bbc98dd79a292e20006faad1d2db685f3e1803
Moritz Breit <mail@mobr.de>
2013-04-12 16:00:21 +0200

    Configure asset handling for production
    - Minify css
    - Add hash to asset filenames

--- config.rb
+++ config.rb
@@ -64,13 +64,14 @@ set :images_dir, 'assets/images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
